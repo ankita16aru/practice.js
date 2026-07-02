6082443284466d6096cd54e68b416167a2ca9ffd
Kumari Akanchha <162492792+ankita16aru@users.noreply.github.com>
2026-07-02 10:02:21 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "45ac"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber)
+//console.log( valueInNumber) //not a number NaN
+
+let isLoggedIn= 3;
+let boolLoggedIn =Boolean(isLoggedIn)
+console.log(boolLoggedIn)
+
+
