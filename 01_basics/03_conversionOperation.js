5188c6c94ee76c083ed37f5d30af5a8b9b425b4c
Kumari Akanchha <162492792+ankita16aru@users.noreply.github.com>
2026-07-02 10:20:09 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,6 +8,18 @@ let valueInNumber = Number(score);
 
 let isLoggedIn= 3;
 let boolLoggedIn =Boolean(isLoggedIn)
-console.log(boolLoggedIn)
+//console.log(boolLoggedIn)
+
+// ********* operations **********
+
+let value=3
+let negvalue= -value
+//console.log(negvalue)
+
+//console.log(2+1)
+//console.log(2*1)
+//console.log(2**1)
+//console.log(2/1)
+//console.log(2%1)
 
 
