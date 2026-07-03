b2f7d1a0985f426ae3d1377864cb133876737f60
Kumari Akanchha <162492792+ankita16aru@users.noreply.github.com>
2026-07-03 07:37:05 +0000

    mathsAndNum

---
+++ 01_basics/07_numAndmaths.js
@@ -0,0 +1,19 @@
+const score =400
+//console.log(score)
+
+const balance =new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+
+const hundred= 100000000
+//console.log(hundred.toLocaleString('en-IN')) 
+
+
+//********MATHS ********
+
+//console.log(Math)
+console.log(Math.abs(-1))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2))
